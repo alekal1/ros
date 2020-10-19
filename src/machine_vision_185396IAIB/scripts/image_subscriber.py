2f5ca7fb0524b206df214f1cb38a501d7ddddc8a
alekal1 <sasha.aleksandov@gmail.com>
2020-10-19 22:27:09 +0300

    5.1

--- src/machine_vision_185396IAIB/scripts/image_subscriber.py
+++ src/machine_vision_185396IAIB/scripts/image_subscriber.py
@@ -17,10 +17,13 @@ bridge = CvBridge()
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
+image_pub = rospy.Publisher("/image_processed", Image, queue_size=1000)
+camera_pub = rospy.Publisher("/camera_info", CameraInfo, queue_size=1000)
+
 
 
 def callback(image_data):
-    global bridge, collected_images, objpoints, imgpoints
+    global bridge, collected_images, objpoints, imgpoints, camera_pub
 
     objp = np.zeros((6*7, 3), np.float32)
     objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2) * 0.108
@@ -39,6 +42,7 @@ def callback(image_data):
             imgpoints.append(corners2)
 
             cv2.drawChessboardCorners(img_data, (7,6), corners2,ret)
+        rospy.loginfo("Total error wil be shown when 37 images will be collected! Images collected: %s", len(collected_images))
 
     if len(collected_images) == 37:
         gray = cv2.cvtColor(img_data,cv2.COLOR_BGR2GRAY)
@@ -48,28 +52,49 @@ def callback(image_data):
 
             err = cv2.norm(imgpoints[i], imgpointsKnown, cv2.NORM_L2) / len(imgpointsKnown)
             error += err
-    rospy.loginfo("Total error wil be shown when 37 images will be collected! (Images collected %s): %s", len(collected_images), error)
+        rospy.loginfo("Total error : %s", error)
 
+        camera_publish(image_data, dist, mtx)
+        image_publish(img_data)
+
+        
+def image_publish(img):
+    global image_pub
+    image = Image()
+    image.header.frame_id = "camera"
+    image.height = len(img)
+    image.width = len(img[0])
+    image.encoding = 'rgb8'
+    image.step = len(img[0]) * 3
+    image.data = np.array(img).tostring()
+    image_pub.publish(image)
+
+def camera_publish(image_data, dist, mtx):
+    global camera_pub
     camera = CameraInfo()
     camera.header.frame_id = "camera"
     camera.D = dist.flatten()
     camera.K = mtx.flatten()
-    camera.R = np.eye(e).flatten()
+    camera.R = np.eye(3).flatten()
     mtx1 = np.zeros((3, 4))
     mtx1[:,:-1] = mtx
     camera.P = mtx1.flatten()
     camera.height = image_data.height
     camera.width = image_data.width
     camera.distortion_model = 'plumb_bob'
-    
 
-def subscriber():
+    camera_pub.publish(camera)
+
+
+def run():
 
     rospy.init_node('camera_calibration', anonymous=True)
 
     rospy.Subscriber('/image_raw', Image, callback)
 
+    rospy.Subscriber("/image_mono", Image, callback)
+
     rospy.spin()
 
 if __name__ == "__main__":
-    subscriber()
+    run()
