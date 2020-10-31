cf4d691cc9d445882028bda003bec9f483d38c5e
alekal1 <sasha.aleksandov@gmail.com>
2020-10-31 19:43:42 +0200

    backup

--- src/machine_vision_185396IAIB/scripts/video_publisher.py
+++ src/machine_vision_185396IAIB/scripts/video_publisher.py
@@ -8,22 +8,25 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class VideoPublisher:
-     
+
     def __init__(self, path):
         self.video = glob.glob(path+'/data/videos/*.mp4')
-        self.publisher = rospy.Publisher('/video_stream', Image, queue_size=100)
-        self.bridge = CvBridge() 
+        self.publisher = rospy.Publisher(
+            '/video_stream', Image, queue_size=100)
+        self.bridge = CvBridge()
         self.frame_img = Image()
         self.cap = cv2.VideoCapture(self.video[0])
 
         self.rate = rospy.Rate(self.cap.get(cv2.CAP_PROP_FPS))
-        self.video_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT) # Get total video frames  [NB! cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT) is not working so it's harcoded]
+        # Get total video frames  [NB! cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT) is not working so it's harcoded]
+        self.video_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
         self.frame_counter = 0
-    
-    def create_frame_image(self, frame):
-        self.frame_img = self.bridge.cv2_to_imgmsg(frame, encoding="passthrough")
 
+    def create_frame_image(self, frame):
+        self.frame_img = self.bridge.cv2_to_imgmsg(
+            frame, encoding="passthrough")
 
     def publish_video_frame(self):
         while (self.cap.isOpened()):
@@ -32,13 +35,14 @@ class VideoPublisher:
             # rospy.loginfo('Frame counter: %s Video Frames: %s', self.frame_counter, self.video_frames)
             if self.frame_counter == self.video_frames:
                 self.frame_counter = 0
-                self.cap = cv2.VideoCapture(self.video[0]) # Recaptures video when it's finished
+                # Recaptures video when it's finished
+                self.cap = cv2.VideoCapture(self.video[0])
             if ret == True:
+
                 self.create_frame_image(frame)
                 self.publisher.publish(self.frame_img)
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
-
 
     def run(self):
         while not rospy.is_shutdown():
--- src/machine_vision_185396IAIB/scripts/video_subscriber.py
+++ src/machine_vision_185396IAIB/scripts/video_subscriber.py
@@ -9,31 +9,37 @@ import random as rng
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class VideoSubscriber:
 
     def __init__(self):
-        self.video_subscriber = rospy.Subscriber('/video_stream', Image, self.callback)
-        self.frame_publisher = rospy.Publisher('/processed_video', Image, queue_size=1000)
+        self.video_subscriber = rospy.Subscriber(
+            '/video_stream', Image, self.callback)
+        self.frame_publisher = rospy.Publisher(
+            '/processed_video', Image, queue_size=1000)
         self.bridge = CvBridge()
-        self.min_value = np.array([20, 160, 160], dtype="uint8") # Minimum color range
-        self.max_value = np.array([25, 255, 255], dtype="uint8") # Maximum color range
-    
+        self.min_value = np.array(
+            [20, 160, 160], dtype="uint8")  # Minimum color range
+        self.max_value = np.array(
+            [25, 255, 255], dtype="uint8")  # Maximum color range
 
     def callback(self, video_frame_data):
-        frame_img = self.bridge.imgmsg_to_cv2(video_frame_data, desired_encoding="passthrough")
+        frame_img = self.bridge.imgmsg_to_cv2(
+            video_frame_data, desired_encoding="passthrough")
         frame_hsv = cv2.cvtColor(frame_img, cv2.COLOR_BGR2HSV)
 
-        duck_mask = cv2.inRange(frame_hsv, self.min_value, self.max_value) # ??? 
+        duck_mask = cv2.inRange(
+            frame_hsv, self.min_value, self.max_value)  # ???
 
-        self.thresh_callback(frame_hsv, duck_mask) 
-        
-        
+        self.thresh_callback(frame_hsv, duck_mask)
+
     def thresh_callback(self, frame_hsv, canny_output):
         # rospy.loginfo("HERE!")
         # canny_output = cv2.Canny(frame_hsv, self.thresh, self.thresh * 2)
 
         # Find contours
-        _, contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, _ = cv2.findContours(
+            canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         contours_poly = [None]*len(contours)
         boundRect = [None]*len(contours)
@@ -46,19 +52,20 @@ class VideoSubscriber:
             boundRect[i] = cv2.boundingRect(contours_poly[i])
             centers[i], radius[i] = cv2.minEnclosingCircle(contours_poly[i])
 
-
-        drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+        drawing = np.zeros(
+            (canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
 
         # Draw contours
         for i in range(len(contours)):
-            color = (255, 255, 0) # Yellow color in RGB
+            color = (255, 255, 0)  # Yellow color in RGB
             cv2.drawContours(drawing, contours, i, color)
-            cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), \
-            (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
-            cv2.circle(drawing, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color, 2)
+            cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])),
+                          (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+            cv2.circle(drawing, (int(centers[i][0]), int(
+                centers[i][1])), int(radius[i]), color, 2)
+
         msg = self.bridge.cv2_to_imgmsg(drawing, encoding='passthrough')
         self.frame_publisher.publish(msg)
-
 
     def run(self):
         while not rospy.is_shutdown():
